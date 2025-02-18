d110447ff09195a7e1631d518ad89f84c9eb8c55
Noman5713 <noman15-5713@diu.edu.bd>
2025-02-18 12:59:40 +0600

    last task

--- lib/main.dart
+++ lib/main.dart
@@ -9,8 +9,8 @@ class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: "Flutter Basic UI - 02",
-        home: Scaffold(
+      title: "Flutter Basic UI - 02",
+      home: Scaffold(
           appBar: AppBar(
             title: Text("Flutter Basic UI - 02"),
             backgroundColor: Colors.blue,
@@ -19,25 +19,67 @@ class App extends StatelessWidget {
               fontSize: 20,
             ),
           ),
-          body: SingleChildScrollView(
-            scrollDirection: Axis.vertical,
-            child: Container(
-              //height: 600,
-              width: 600,
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: List.generate(
-                  100,
-                  (index) => Icon(
-                    Icons.star,
-                    color: const Color.fromARGB(255, 13, 10, 10),
-                    size: index.toDouble(),
-                  ),
+          // body: SingleChildScrollView(
+          //   scrollDirection: Axis.vertical,
+          //   child: Container(
+          //     //height: 600,
+          //     width: 600,
+          //     child: Column(
+          //       mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          //       crossAxisAlignment: CrossAxisAlignment.center,
+          //       children: List.generate(
+          //         100,
+          //         (index) => Icon(
+          //           Icons.star,
+          //           color: const Color.fromARGB(255, 13, 10, 10),
+          //           size: index.toDouble(),
+          //         ),
+          //       ),
+          //     ),
+          //   ),
+          // ),
+          body: Container(
+            margin: EdgeInsets.all(20),
+            padding: EdgeInsets.all(20),
+            child: Column(
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      "50%off",
+                      style: TextStyle(fontSize: 20),
+                    ),
+                    Icon(
+                      Icons.heart_broken,
+                      size: 30,
+                    )
+                  ],
                 ),
-              ),
+                Text(
+                  "Image",
+                  style: TextStyle(fontSize: 100),
+                ),
+                Text(
+                  "Nike Air Shoes",
+                  style: TextStyle(fontSize: 40),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      "\$450",
+                      style: TextStyle(fontSize: 30),
+                    ),
+                    Text(
+                      "\$500",
+                      style: TextStyle(fontSize: 20),
+                    )
+                  ],
+                )
+              ],
             ),
-          ),
-        ));
+          )),
+    );
   }
 }
