29f3c03e35697ad57c8a6e723e77ac55e4f4ba06
Noman5713 <noman15-5713@diu.edu.bd>
2025-02-18 10:07:18 +0600

    before_task

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,56 @@
-import 'package:flutter/material.dart';
-
+import 'package:flutter/material.dart'; 
 void main() {
-  runApp(const LabClass05());
-
-//Based on state
-//1.stateful - Change/Render /Refresh
-//2.Stateless widget -Fix after render / No 
-
-
+  runApp(App());
 }
-
-class LabClass05 extends StatelessWidget{
-  cons LabClass
+ 
+class App extends StatelessWidget {
+  const App({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Basic UI - 02",
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Basic UI - 02"),
+          backgroundColor: Colors.blue,
+          titleTextStyle: TextStyle(
+            color: Colors.white,
+            fontSize: 20,
+          ),
+        ),
+        body: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            Icon(
+              Icons.ac_unit,
+              size: 50,
+              color: Colors.blueGrey,
+            ),
+            Text(
+              "Hi",
+              style: TextStyle(
+                color: Colors.red,
+                fontSize: 30,
+              ),
+            ),
+            Text(
+              "This is",
+              style: TextStyle(
+                color: Colors.black,
+                fontSize: 30,
+              ),
+            ),
+            Text(
+              "Flutter",
+              style: TextStyle(
+                color: Colors.blue,
+                fontSize: 30,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
 }
-
+ 
